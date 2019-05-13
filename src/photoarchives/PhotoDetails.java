84088c7e8d46825c9ea44be7b5205ca20e6918c3
montgomerya21 <montgomerya21@hanover.edu>
2019-05-13 15:42:09 -0400

    added PhotoDetails class and test

---
+++ src/photoarchives/PhotoDetails.java
@@ -0,0 +1,15 @@
+package photoarchives;
+
+public class PhotoDetails {
+  private Photo photo;
+
+
+
+  public PhotoDetails(Photo photo) {
+    this.photo = photo;
+  }
+
+  public Photo getPhoto() {
+    return photo;
+  }
+}
