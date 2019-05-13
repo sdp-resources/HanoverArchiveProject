85869f243ec2245b26fae16d7c5a9f6bd49bb80e
montgomerya21 <montgomerya21@hanover.edu>
2019-05-13 15:45:34 -0400

    added PhotoDetails class and test

--- test/PhotoDetailTest.java
+++ test/PhotoDetailTest.java
@@ -6,11 +6,11 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PhotoDetailTest {
-  
+
 
   @Test
   public void containsPhoto() {
-    Photo photo = new Photo("dir", "PhotoName");
+    Photo photo = new Photo("dir", "PhotoName", "photoID");
     PhotoDetails photodetails = new PhotoDetails(photo);
     assertThat(photodetails.getPhoto(), is(photo));
 
