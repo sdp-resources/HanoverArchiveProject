47fa7080f5c23945a56c1a954169014dd84e7365
Jonathan Helmers <helmersj20@hanover.edu>
2019-05-13 14:23:32 -0400

    Added getters and setters for photo

--- src/photoarchives/Photo.java
+++ src/photoarchives/Photo.java
@@ -1,3 +1,23 @@
 package photoarchives;
 
-public class Photo {}
+public class Photo {
+  private String source;
+  private String title;
+
+  public Photo(String source, String title) {
+    this.source = source;
+    this.title = title;
+  }
+
+  public String getSource() {
+    return this.source;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+
+  public void setTitle(String newTitle) {
+    this.title = newTitle;
+  }
+}
--- test/PhotoTest.java
+++ test/PhotoTest.java
@@ -1,9 +1,27 @@
+import org.junit.Before;
 import org.junit.Test;
 import photoarchives.Photo;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class PhotoTest {
+  private Photo photo;
+
+  @Before
+  public void setUp() throws Exception {
+    String source = "source";
+    String title = "title";
+    photo = new Photo(source, title);
+  }
   @Test
   public void makePhoto() throws Exception {
-    Photo photo = new Photo();
+    assertEquals("source", photo.getSource());
+    assertEquals("title", photo.getTitle());
+  }
+  @Test
+  public void editTitle() throws Exception {
+    String newTitle = "new title";
+    photo.setTitle(newTitle);
+    assertEquals(newTitle, photo.getTitle());
   }
 }
