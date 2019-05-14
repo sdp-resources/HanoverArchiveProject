29e3265f9b56171acada57739a06b3b59cba307e
Terriyon <terriyonyaya11@gmail.com>
2019-05-14 14:44:58 -0400

    Added functions to photoArchive

--- src/photoarchives/PhotoArchive.java
+++ src/photoarchives/PhotoArchive.java
@@ -9,4 +9,19 @@ public class PhotoArchive {
     this.photos = photos;
   }
 
+  public String getName() {
+    return this.name;
+  }
+
+  public PhotoList getPhotoList() {
+    return this.photos;
+  }
+
+  public void setName(String newName) {
+    this.name = newName;
+  }
+
+  public void addPhotoToArchive(Photo newPhoto) {
+    this.photos.addPhoto(newPhoto);
+  }
 }
--- test/PhotoArchiveTest.java
+++ test/PhotoArchiveTest.java
@@ -1,12 +1,28 @@
 import org.junit.Test;
+import photoarchives.Photo;
 import photoarchives.PhotoArchive;
 import photoarchives.PhotoList;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class PhotoArchiveTest {
   private PhotoList photo;
+  PhotoArchive photoArchive = new PhotoArchive("dog", photo);
+  private Photo blankPhoto = new Photo();
   @Test
-  public void CreatePhotoArchiveDirectoryTest(){
-    PhotoArchive photoArchive = new PhotoArchive("dog",photo);
-    AssertEquals(photoArchive.getName(), "dog");
+  public void PhotoArchiveKnowsNameTest() {
+    assertEquals("dog", photoArchive.getName());
+    assertEquals(photo, photoArchive.getPhotoList());
+  }
+  @Test
+  public void ChangeNameOfPhotoArchive() {
+    photoArchive.setName("cat");
+    assertEquals("cat", photoArchive.getName());
+  }
+  @Test
+  public void addPhotoToPhotoArchivePhotoList() {
+    photoArchive.addPhotoToArchive(blankPhoto);
+    assertEquals(1, photoArchive.getPhotoList().getLength());
   }
 }
+
