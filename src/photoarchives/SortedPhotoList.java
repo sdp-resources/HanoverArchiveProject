fb1893710ee4d5a205c254fc5386fcd388e5f2f3
Tyler Walker <walkert20@hanover.edu>
2019-05-17 15:30:11 -0400

    Modifications to SortedPhotoList

--- src/photoarchives/SortedPhotoList.java
+++ src/photoarchives/SortedPhotoList.java
@@ -1,5 +1,13 @@
 package photoarchives;
+import java.util.*;
 
 public class SortedPhotoList extends PhotoList{
 
+  public boolean canSortList(PhotoList photos) {
+    return photos.getLength() > 1;
+  }
+
+  public PhotoLabel compareTo(Photo photo1, Photo photo2){
+    return photo1.getLabels().compareTo(photo2.getLabels());
+  }
 }
