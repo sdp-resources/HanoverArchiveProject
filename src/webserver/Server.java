01bada1b2f8c1da81884f087d7db8f9a8df1fc0f
Tyler Walker <walkert20@hanover.edu>
2019-05-21 14:37:16 -0400

    Created favicon.ico and added in the start of the photo list display in our server.java and PhotoList.handlebars

--- src/webserver/Server.java
+++ src/webserver/Server.java
@@ -1,23 +1,28 @@
 package webserver;
 
+import photoarchives.PhotoArchive;
+import photoarchives.PhotoList;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
-import java.util.HashMap;
+import java.util.*;
 
 import static spark.Spark.*;
 
 // By default serving at http://localhost:4567
 public class Server {
 
+  private static String archiveDir;
+  private static PhotoArchive photoArchive;
   private static HandlebarsTemplateEngine templateEngine = new HandlebarsTemplateEngine();
 
   public static void main(String[] args) {
     staticFiles.location("/public");
     get("/", Server::serveIndex);
     post("/login", Server::processLogin);
+    get("/photos", Server::showPhotoList);
   }
 
   private static Object serveIndex(Request req, Response res) {
@@ -30,8 +35,16 @@ public class Server {
     return "null";
   }
 
+  private static Object showPhotoList(Request req, Response res) {
+    // TODO: Get this displaying photos
+    archiveDir = "assets/public/image_archive";
+    photoArchive = new PhotoArchive("Archive", archiveDir);
+    HashMap<Object, Object> returnedHash = new HashMap<>();
+    returnedHash.put("values", photoArchive);
+    return serveTemplate("/profilesList.handlebars", returnedHash);
+  }
+
   private static String serveTemplate(String templatePath, HashMap<Object, Object> model) {
     return templateEngine.render(new ModelAndView(model, templatePath));
   }
-
 }
