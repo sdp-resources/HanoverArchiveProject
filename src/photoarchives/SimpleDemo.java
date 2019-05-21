1db0a2c8a37314588ade355ffcf4b44560462fb3
twilson-hanover <wilsont@hanover.edu>
2019-05-21 15:40:17 -0400

    Revised SimpleDemo

--- src/photoarchives/SimpleDemo.java
+++ src/photoarchives/SimpleDemo.java
@@ -3,34 +3,36 @@ package photoarchives;
 import java.util.Scanner;
 
 public class SimpleDemo  {
-  private String archiveDir;
-  public PhotoArchive photoArchive;
-
-  public SimpleDemo() {
-    archiveDir = "assets/public/image_archive";
-    photoArchive = new PhotoArchive("demo", archiveDir);
-    photoArchive.initialize();
-  }
+  public static final String ARCHIVE_NAME = "Hanover Photo Archive";
+  public static final String IMAGE_DIR = "/tmp/image_archive";
+  public static PhotoArchive photoArchive = new PhotoArchive(ARCHIVE_NAME, IMAGE_DIR);
 
   public void runDemo() {
-    Photo demoPhoto = new Photo("demoPhoto");
-    demoPhoto.setUploadedFrom("images/demoImage.jpg");
-    photoArchive.addPhotoToArchive(demoPhoto);
-    System.out.println("Our Archive has " + photoArchive.getPhotoList().getLength() + " photo!");
-    printLabels(demoPhoto);
+    photoArchive.initialize();
+    System.out.println(ARCHIVE_NAME + " created!");
+    addPhotoFromUser();
+    System.out.println("\nCurrent Archive:\n");
+    printPhotoList();
+    //System.out.println("Our Archive has " + photoArchive.getPhotoList().getLength() + " photo!");
     // Keep demo running so archive files can be checked (ugly comment)
-    //waitForEnter();
-    addPhoto();
+    waitForEnter();
   }
 
-  public void printLabels(Photo photo){
+  public void printLabels(Photo photo) {
     photo.addField(PhotoField.Kind.LOCATION, "Hanover");
     photo.addField(PhotoField.Kind.TITLE, "Title");
     photo.addField(PhotoField.Kind.SUBJECT, "Sports");
     for (PhotoField label : photo.getFields()) {
       System.out.println(label.getKind() + ": " + label.getValue());
     }
+  }
 
+  public void printPhotoList() {
+    for (Photo photo : photoArchive.getPhotoList()) {
+      System.out.print("photo id: " + photo.getID() + " ");
+      System.out.print("title: " + photo.getFieldValue(PhotoField.Kind.TITLE) + " ");
+      System.out.println("image location: " + photo.getImageLocation());
+    }
   }
 
   private void waitForEnter() {
@@ -41,14 +43,21 @@ public class SimpleDemo  {
     }
   }
 
-  public void addPhoto(){
+  public void addPhotoFromUser() {
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Next Photo: ");
-    String photoLocation = scanner.next();
-    while(!(photoLocation.equals("no"))){
-      System.out.print("Next Photo: ");
-      photoLocation = scanner.next();
-    }
+    Photo photo = new Photo(getURL(scanner));
+    photo.addField(PhotoField.Kind.TITLE, getTitle(scanner));
+    photoArchive.addPhoto(photo);
+  }
+
+  public String getURL(Scanner scanner) {
+    System.out.print("Photo URL? ");
+    return scanner.next();
+  }
+
+  public String getTitle(Scanner scanner) {
+    System.out.print("Photo Title? ");
+    return scanner.next();
   }
 
   public static void main(String[] args){
