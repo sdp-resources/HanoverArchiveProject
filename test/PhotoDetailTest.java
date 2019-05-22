4a6ba67c498a752329b92f777682c55e9fbb3a76
Tyler Walker <walkert20@hanover.edu>
2019-05-22 16:21:52 -0400

    Refactored to get rid of unnecessary test and changed test format

--- test/PhotoDetailTest.java
+++ test/PhotoDetailTest.java
@@ -1,19 +1,15 @@
 import org.junit.Test;
 import photoarchives.PhotoDetails;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class PhotoDetailTest {
 
-
   @Test
-  public void canCreateDetails() {
+  public void canCreateDetails_createsEmptyList() {
     PhotoDetails details = new PhotoDetails();
-  }
-  @Test
-  public void createsEmptyList() {
-    PhotoDetails details = new PhotoDetails();
-    assertThat(details.isEmpty(), is(true));
+    assertTrue(details.isEmpty());
   }
 }
